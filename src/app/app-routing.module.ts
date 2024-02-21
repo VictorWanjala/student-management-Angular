8d2de576b86450f02ec9345a283f4149a112b85d
Victor <wanjala94vic@gmail.com>
2024-02-21 17:52:49 +0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './comps/add-student/add-student.component';
+import { EditStudentComponent } from './comps/edit-student/edit-student.component';
+import { LoginStudentComponent } from './comps/login-student/login-student.component';
+import { RegistrationStudentComponent } from './comps/registration-student/registration-student.component';
+import { ListStudentComponent } from './comps/list-student/list-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add',
+  component:AddStudentComponent
+  },
+  {path: 'login',
+  component:LoginStudentComponent
+  },
+  {path: 'edit',
+  component:EditStudentComponent
+  },
+  {path: 'register',
+  component:RegistrationStudentComponent
+  },
+  {path: 'list',
+  component:ListStudentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './comps/add-student/add-student.component';
+import { EditStudentComponent } from './comps/edit-student/edit-student.component';
+import { ListStudentComponent } from './comps/list-student/list-student.component';
+import { LoginStudentComponent } from './comps/login-student/login-student.component';
+import { RegistrationStudentComponent } from './comps/registration-student/registration-student.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    ListStudentComponent,
+    LoginStudentComponent,
+    RegistrationStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
